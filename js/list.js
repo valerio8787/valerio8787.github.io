62e661a955622a66b8bad3a7709907bd1bea9f3d
Valerii Pravoslavnyi <valerio8787@gmail.com>
2017-01-04 16:35:55 +0200

    added descs

--- js/list.js
+++ js/list.js
@@ -1,20 +1,20 @@
 $(function(){
 	for (var i in routes){
-		$('#route-list').append('<div class="panel panel-default"><div class="panel-heading" role="tab" id="headingOne"><h4 class="panel-title">' 
+		$('#route-list').append('<div class="panel panel-default"><div class="panel-heading" role="tab" id="headingOne"><h4 class="panel-title">'
         + '<a data-toggle="collapse" data-parent="#accordion" href="#route' + routes[i].routeID + '" aria-expanded="true" aria-controls="route' + routes[i].routeID + '">'
-        + routes[i].title 
+        + routes[i].title
         + '</a></h4> </div><div id="route' + routes[i].routeID + '" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">'
 		+ '</div></div>');
 		for (key in routes[i].places)
-    	{    	
-    		text = "";
-    		if (descriptions[routes[i].places[key].id] != undefined)
+    	{
+    		var text = "";
+				var id = routes[i].places[key].id;
+    		if (descriptions[id] != undefined)
     		{
-    			text = descriptions[routes[i].places[key].id].text;
+    			text = descriptions[id].text;
     		}
-    		$("#route" + routes[i].routeID).append('<div class="panel panel-default"><div class="panel-heading"><h2>' + routes[i].places[key].title + '</h2></div>' 
+    		$("#route" + routes[i].routeID).append('<div class="panel panel-default"><div class="panel-heading"><h2>' + routes[i].places[key].title + '</h2></div>'
 				  +'<div class="panel-body">' + text + '</div></div>');
     	}
 	}
 });
-	
